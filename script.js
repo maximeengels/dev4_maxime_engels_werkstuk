ce33400ae8081f20ce05a6231b09b70ffc2c2358
Maxime Engels <maxime.engels@student.ehb.be>
2020-08-29 01:16:26 +0200

    search function + search result card WIP

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 
+let query;
+
 fetch('data/entries.json')
   .then(response => response.json())
   .then(data => {
     const cards = document.getElementById('cards');
-    console.log(data.items);
     data.items.map( item => {
     cards.insertAdjacentHTML('afterbegin', 
-      `<div class="card-container" id="${item.category}">
+      `<div class="card-container ${item.category}">
         <a href="/" class="video-card">
           <div class="video-card-img">
             <img src="${item.thumbnail.url}" alt="">
@@ -19,24 +20,84 @@ fetch('data/entries.json')
           <div class="video-card-content">
             <h3 class="name">${item.name}</h3>
             <span class="discription company">${item.excerpt}</span>
-            <span class="discription location">${item['recorded-at']}</span>
-            <span class="duration">${item['video-length']}</span>
+            <span class="discription location">${filterEmptyData(item['recorded-at'])}</span>
+            <span class="duration">${filterEmptyData(item['video-length'])}</span>
+          </div>
+        </a>
+      </div>`
+    )}
+  );
+});
+
+
+document.getElementById('search').onkeyup = function(){
+  query = this.value;
+  console.log(query);
+  
+  addSearchCards();
+};
+
+function addSearchCards() {
+  fetch('data/entries.json')
+  .then(response => response.json())
+  .then(data => {
+    const searchcards = document.querySelector('.search-container');
+    data.items.filter(filterQuery).map( item => {
+      searchcards.insertAdjacentHTML('beforeend', 
+      `<div class="card-container ${item.category}">
+        <a href="/" class="video-card">
+          <div class="video-card-img">
+            <img src="${item.thumbnail.url}" alt="">
+            <div class="inner">  
+              <div class="video-card-img-tag">
+                <span class="tagline">${item['genre-v2']}</span>
+              </div>
+            </div>
+          </div>
+          <div class="video-card-content">
+            <h3 class="name">${item.name}</h3>
+            <span class="discription company">${item.excerpt}</span>
+            <span class="discription location">${filterEmptyData(item['recorded-at'])}</span>
+            <span class="duration">${filterEmptyData(item['video-length'])}</span>
           </div>
         </a>
       </div>`
       )}
     );
   });
+}
 
-  function filterCategory(cat) {
-    console.log(cat);
-    // if(cat != ){
-
-    // }
+function filterQuery(item){
+  if ('name' in item && item.name == query) {
+    return true;
+  } else {
+    return false;
   }
+}
 
-  // fetch(apiUrl+'/items', settings)
-//   .then(res => res.json())
-//   .then((json) => {
-//     console.log(`Got the response, there are ${json.items.length} entries`)
-//   });
+function filterEmptyData(data){
+  if(data == undefined){
+    return "<br>";
+  } else{
+    return data;
+  }
+}
+
+function clickEvent(id){
+  let value = document.getElementById(id).value;
+  document.getElementById(id).classList.toggle("active");
+  console.log(value);
+  document.querySelectorAll('.familie').style.display = "none";
+}
+
+
+  document.querySelector('button').onclick = function () {
+    clickEvent(this.id);
+    // this.classList.toggle("active");
+    // document.querySelector('.familie').style.display = "none";
+}
+
+//   document.querySelector('.fam').onclick = function () {
+//     this.classList.toggle("active");
+//     document.querySelector('.volwassenen').style.display = "none";
+// }
