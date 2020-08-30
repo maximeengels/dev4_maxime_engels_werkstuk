959c4ac4f8c4e56a8c6724ae17bd916f42d955d6
Maxime Engels <maxime.engels@student.ehb.be>
2020-08-30 04:06:46 +0200

    Filter function works + search "no result found" added

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
-(function() {
+(async function () {
 
-let query;
-asyncCall();
+  const searchcards = document.querySelector('.search-container');
+  const filters = document.querySelectorAll('.filter-btn');
+  const cards = document.getElementById('cards');
 
-async function asyncCall() {
-  getData();
-}
-
-async function getData() {
+  let query;
+  let arrayItems;
+  let filterArray;
+  let entries;
+// fetches json file and activates function which passes entries through
   await fetch('data/entries.json')
     .then(response => response.json())
-    .then(data => {
-      const cards = document.getElementById('cards');
-      data.items.map( item => {
-      cards.insertAdjacentHTML('afterbegin', 
+    .then((data) => {
+      entries = data.items;
+      addCards(entries);
+    });
+
+// function adds item cards to the page using array parameter
+  async function addCards(data) {
+    data.map(item => {
+      cards.insertAdjacentHTML('afterbegin',
         `<div class="card-container ${item.category}">
           <a href="/" class="video-card">
             <div class="video-card-img">
@@ -32,27 +38,24 @@ async function getData() {
             </div>
           </a>
         </div>`
-      )}
-    );
-  });
-}
+      )
+    });
+  }
 
-document.getElementById('search').onkeyup = async function(){
-  query = this.value;
-  await addSearchCards();
-};
+// key-up event triggers when typing in search bar
+  document.getElementById('search').onkeyup = async function () {
+    query = this.value;
+    await addSearchCards();
+  }
 
-function addSearchCards() {
-  fetch('data/entries.json')
-  .then(response => response.json())
-  .then(data => {
-    const searchcards = document.querySelector('.search-container');
-    if(query !== ""){
+// function adds cards that match typed search query
+  function addSearchCards() {
+    if (query !== "") {
       searchcards.innerHTML = "";
-      let arrayItems = data.items.filter(filterQuery);
-      arrayItems.map( item => {
-        searchcards.insertAdjacentHTML('afterbegin', 
-        `<div class="search-card-container">
+      arrayItems = entries.filter(filterQuery);
+      arrayItems.map(item => {
+        searchcards.insertAdjacentHTML('afterbegin',
+          `<div class="search-card-container">
           <a href="/" class="search-card video-card">
             <div class="search-video-card-img">
               <img src="${item.thumbnail.url}" alt="">
@@ -63,37 +66,83 @@ function addSearchCards() {
             </div>
           </a>
         </div>`
-        )}
-      );
+        )
+      });
     } else {
       searchcards.innerHTML = "";
     }
-  });
-}
+    if (arrayItems.length === 0 && query.length !== 0) {
+      searchcards.innerHTML = "";
+      searchcards.insertAdjacentHTML('afterbegin',
+      `<div class="search-card-container">
+      <h2>Geen resultaten voor "${query}".</h2>
+      </div>`);
+    }
+  }
 
-function filterQuery(item){
-    //regex checks if 
+// filters search query
+  function filterQuery(item) {
+    //regex checks if query matches item name, item name has to 
+    //start with query letter and looks at global and case insensitive flags
     const regex = new RegExp(`^${query}`, 'gi');
     return item.name.match(regex);
-}
-
-function filterEmptyData(data){
-  if(data == undefined){
-    return "<br>";
-  } else{
-    return data;
   }
-}
+// filters undefined data from json file
+  function filterEmptyData(data) {
+    if (data == undefined) {
+      return "<br>";
+    } else {
+      return data;
+    }
+  }
+// filters genre per item in array using item and filterArray in parameters
+  const filterGenre = (tag, filterArray) => {
+    for (item of filterArray) {
+      if (tag['genre-v2'] == item) {
+        return true;
+      }
+    }
+  }
+// filters category per item in array using item and filterArray in parameters
+  const filterCategory = (tag, filterArray) => {
+    for (item of filterArray) {
+      if (tag['category'] == item) {
+        return true;
+      }
+    }
+  }
 
-function clickEvent(id){
-  let value = document.getElementById(id).value;
-  document.getElementById(id).classList.toggle("active");
-  console.log(value);
-  document.querySelectorAll('.familie').style.display = "none";
-}
+// toggles active class of filter buttons and passes array if all active ones
+  for (button of filters) {
+    button.onclick = function () {
+      this.classList.toggle("active");
+      const activeFilters = document.querySelectorAll('.active');
+      tagFilter(activeFilters);
+    }
+  }
 
-document.querySelector('button').onclick = async function () {
-  clickEvent(this.id);
-}
+// filters data or entries using if else statements and .filter()
+// which activates functions that check which tags are active
+  const tagFilter = (activeFilters) => {
+    filterArray = [];
+    let genreArray = entries.filter(item => filterGenre(item, filterArray));
+    let categoryArray = entries.filter(item => filterCategory(item, filterArray));
+
+    for (filterName of activeFilters) {
+      filterArray.push(filterName.id);
+    }
+
+    cards.innerHTML = '';
+
+    if (filterArray == '') {
+      addCards(entries);
+    } else if (genreArray == '') {
+      addCards(categoryArray);
+    } else if (categoryArray == '') {
+      addCards(genreArray);
+    } else {
+      addCards(genreArray.filter(item => filterCategory(item, filterArray)));
+    }
+  }
 
 })();
